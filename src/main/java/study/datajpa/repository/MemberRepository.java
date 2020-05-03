4b0386db2c3e6f4c0bcebaddeb024caac3423465
jiwon615 <seg615@naver.com>
2020-05-03 17:55:48 +0900

    @Query, 값, DTO 조회하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -13,5 +14,13 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // 만약 위 메소드명에 조건이 훨씬 많이 점점 길어지면 한없이 길어지는 단점.. 그것을 극복위해 바로 JPQL 아래처럼 작성 가능
     @Query("select m from Member m where m.username = :username and m.age = :age")
-    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);  // 메소드명은 아무렇게나!
+
+    // 값 조회
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    // DTO 조회
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
